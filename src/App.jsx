d156e47914964b2147d304f5867c2194f227cf86
Zohaib <naparzohaibkhan@gmail.com>
2025-10-17 09:52:56 +0500

    changing in new page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header/Header";
 import HeroSection from "./components/HeroSection/HeroSection";
 import AboutMe from "./components/AboutMe/AboutMe";
+import Education from "./components/Education/Education";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <HeroSection />
       <AboutMe />
+      <Education />
     </>
   );
 };
