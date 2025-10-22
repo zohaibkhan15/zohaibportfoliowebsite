cb831f2c0b2be8c6690341aa38625b89413c32ba
Zohaib <naparzohaibkhan@gmail.com>
2025-10-23 01:00:01 +0500

    cammand

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
-import HeroSection from './components/HeroSection/HeroSection'
-import Header from './components/Header/Header'
-import AboutMe from './components/AboutMe/AboutMe'
-import Education from './components/Education/Education'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-      <HeroSection />
-      <Header />
-      <AboutMe />
-      <Education />
-
-
-    </div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
